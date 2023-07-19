ed104c1e3c9fff97b8f5919e058fc0a70c0fac3d
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2023-07-19 11:19:10 +0300

    Remove src and theme folders.

--- src/__stories__/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import Index from "../pages/index"
-import About from "../pages/about"
-
-storiesOf(`index.js`, module).add(`default`, () => <Index />)
-
-storiesOf(`index.js`, module).add(`about`, () => <About />)
--- src/pages/404.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./index.scss"
-
-export default () => (
-  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-    <h1>Nothing here!</h1>
-  </div>
-)
